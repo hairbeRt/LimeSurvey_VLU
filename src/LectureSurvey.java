65bcf349ef38794ac73b7e7e60ba4cd21cd8c587
Jonas C <32942742+hairbeRt@users.noreply.github.com>
2021-06-09 16:04:43 +0200

    Encoding-geplänkel

--- src/LectureSurvey.java
+++ src/LectureSurvey.java
@@ -31,7 +31,7 @@ public class LectureSurvey {
 	
 	public String toTex() {
 		StringBuilder result = new StringBuilder();
-		//Preamble
+		// Preamble
 		result.append(
 				"\\documentclass[a4paper]{scrartcl}\r\n\\usepackage[utf8]{inputenc}\r\n\\usepackage[ngerman]{babel}\r\n\\usepackage{graphicx}\r\n%\\usepackage{array}\r\n\\usepackage{enumitem}\r\n\\usepackage{listings}\r\n\\usepackage{color}\r\n\\usepackage{pdfpages}\r\n\\usepackage{tabularx}\r\n\r\n\\usepackage[fleqn]{amsmath}\r\n\\usepackage{amssymb}\r\n\r\n\\usepackage{geometry}\r\n\\geometry{margin=2cm}\r\n\r\n\\setlength{\\parskip}{0.5em}\r\n\\setlength{\\parindent}{0em}\r\n\r\n\\usepackage[colorlinks=true]{hyperref}\r\n\r\n\\usepackage{pgfplots}\r\n\\pgfplotsset{compat=1.8}\r\n\\pgfplotsset{\r\n    ybar,\r\n    xtick=data,\r\n    nodes near coords,\r\n    nodes near coords align={vertical},\r\n    width=\\linewidth,\r\n    height=7.5em,\r\n    xticklabel style={align=center, text width=2cm},\r\n}\r\n\r\n\\pagestyle{empty}\r\n\r\n\\newcommand{\\he}[1]{\\textbf{\\small #1}}\r\n\\newcommand{\\tableHeadTypEins}{ & \\he{nie} & \\he{selten} & \\he{manchmal} & \\he{meistens} & \\he{immer} & \\he{nicht anwendbar} \\\\\\hline}\r\n\\newcommand{\\tableHeadTypZwei}{ & \\he{nie} & \\he{selten} & \\he{manchmal} & \\he{meistens} & \\he{immer} \\\\\\hline}\r\n\\newcommand{\\framedText}[1]{\\noindent\\framebox{\\begin{minipage}{\\linewidth}#1\\end{minipage}}}\r\n\\def\\arraystretch{1.33}\r\n\r\n%---------------------------------------------------------------------------------------------------\r\n\r\n\\newcommand{\\qBarChartQuestion}[1]{\\begin{minipage}{.215\\linewidth}#1\\end{minipage}\\hspace{.02\\linewidth}}\r\n\r\n\\newcommand{\\setScalaNewerToAlways}{\\pgfplotsset{symbolic x coords={nie ,selten ,manchmal ,meistens ,immer ,nicht anwendbar ,}, ylabel={\\#votes}}}\r\n\r\n\\newenvironment{qBarChart}[1]{\\qBarChartQuestion{ #1}\\begin{minipage}{.765\\linewidth}\\begin{tikzpicture}\\setScalaNewerToAlways{}\\tikzstyle{every node}=[font=\\small]}{\\end{tikzpicture}\\end{minipage}}\r\n\r\n\r\n\r\n\\newcommand{\\rBarChartQuestion}[1]{\\begin{minipage}{.15\\linewidth}#1\\end{minipage}\\hspace{.02\\linewidth}}\r\n\r\n\\newcommand{\\setScalaBadToGood}{\\pgfplotsset{symbolic x coords={Wurde nicht eingesetzt ,sehr schlecht ,schlecht ,befriedigend ,gut ,sehr gut ,}, ylabel={\\#votes},}}\r\n\r\n\\newenvironment{rBarChart}[1]{\\rBarChartQuestion{ #1}\\begin{minipage}{.83\\linewidth}\\begin{tikzpicture}\\setScalaBadToGood{}\\tikzstyle{every node}=[font=\\small]}{\\end{tikzpicture}\\end{minipage}}\r\n\r\n\r\n");
 		
@@ -41,54 +41,54 @@ public class LectureSurvey {
 		result.append("%==============================================================================\r\n\\section{Bewertung der Vorlesung}\r\n%==============================================================================\n");
 		result.append(questions.get("Wie oft hast du die Vorlesung besucht?").toTex());
 		result.append(questions.get("Wurden Themen durch Beispiele veranschaulicht?").toTex());
-		result.append(questions.get("Wurden die Themen ausf¸hrlich genug erkl‰rt?").toTex());
+		result.append(questions.get("Wurden die Themen ausf√ºhrlich genug erkl√§rt?").toTex());
 		result.append(questions.get("War die Struktur der Vorlesung klar zu erkennen?").toTex());
 		result.append(questions.get("Waren die Folien/das Skript hilfreich?").toTex());
 		
 		//Bewertung der Dozierenden
 		result.append("%==============================================================================\r\n\\section{Bewertung der Dozierenden}\r\n%==============================================================================\n");
 		result.append(questions.get("Die Geschwindigkeit der Vorlesung war...").toTex());
-		result.append(questions.get("Wie viel verstehst du w‰hrend der Vorlesung?").toTex());
+		result.append(questions.get("Wie viel verstehst du w√§hrend der Vorlesung?").toTex());
 		result.append(questions.get("Ist der Dozent/die Dozentin gut auf Fragen eingegangen?").toTex());
-		result.append(questions.get("War der Dozent/die Dozentin auﬂerhalb der Vorlesung f¸r Fragen etc. erreichbar?").toTex());
+		result.append(questions.get("War der Dozent/die Dozentin au√üerhalb der Vorlesung f√ºr Fragen etc. erreichbar?").toTex());
 		result.append(questions.get("War die Dozentin / der Dozent akustisch gut zu verstehen?").toTex());
 		
 		//Bewertung des Moduls
 		result.append("%==============================================================================\r\n\\section{Bewertung des Moduls}\r\n%==============================================================================\n");
-		result.append(questions.get("Helfen die verlangten Studienleistungen, das Modul erfolgreich abzuschlieﬂen?").toTex());
-		result.append(questions.get("Findest du die verlangten Studienleistungen f¸r dieses Modul angemessen?").toTex());
-		result.append(questions.get("W¸rdest du das Modul weiterempfehlen?").toTex());
+		result.append(questions.get("Helfen die verlangten Studienleistungen, das Modul erfolgreich abzuschlie√üen?").toTex());
+		result.append(questions.get("Findest du die verlangten Studienleistungen f√ºr dieses Modul angemessen?").toTex());
+		result.append(questions.get("W√ºrdest du das Modul weiterempfehlen?").toTex());
 		result.append(questions.get("Der Praxisbezug war...").toTex());
-		result.append(questions.get("Ist der Arbeitsaufwand f¸r dieses Modul im Hinblick auf die LP-Zahl angemessen?").toTex());
-		result.append(questions.get("Dein Interesse f¸r dieses Thema ist...").toTex());
-		result.append(questions.get("Wie viele Stunden hast du insgesamt, inkl. Vorlesung, ‹bung, ‹bungsaufgaben\\dots, pro Woche f¸r dieses Modul aufgewendet?").toTex());
+		result.append(questions.get("Ist der Arbeitsaufwand f√ºr dieses Modul im Hinblick auf die LP-Zahl angemessen?").toTex());
+		result.append(questions.get("Dein Interesse f√ºr dieses Thema ist...").toTex());
+		result.append(questions.get("Wie viele Stunden hast du insgesamt, inkl. Vorlesung, √úbung, √úbungsaufgaben\\dots, pro Woche f√ºr dieses Modul aufgewendet?").toTex());
 		
-		//Bewertung der ‹bungsaufgaben
-		result.append("%==============================================================================\r\n\\section{Bewertung der ‹bungsaufgaben}\r\n%==============================================================================\n");
-		result.append(questions.get("Wie oft hast du die ‹bungen besucht?").toTex());
-		result.append(questions.get("Wurden die ‹bungsaufgaben rechtzeitig zur Verf¸gung gestellt?").toTex());
-		result.append(questions.get("Die Schwierigkeit der ‹bungsbl‰tter schwankte...").toTex());
+		//Bewertung der √úbungsaufgaben
+		result.append("%==============================================================================\r\n\\section{Bewertung der √úbungsaufgaben}\r\n%==============================================================================\n");
+		result.append(questions.get("Wie oft hast du die √úbungen besucht?").toTex());
+		result.append(questions.get("Wurden die √úbungsaufgaben rechtzeitig zur Verf√ºgung gestellt?").toTex());
+		result.append(questions.get("Die Schwierigkeit der √úbungsbl√§tter schwankte...").toTex());
 		result.append(questions.get("Die Vorlesung war...").toTex());
-		result.append(questions.get("Die ‹bungsgruppe war...").toTex());
-		result.append(questions.get("Die ‹bungsaufgaben waren meistens...").toTex());
+		result.append(questions.get("Die √úbungsgruppe war...").toTex());
+		result.append(questions.get("Die √úbungsaufgaben waren meistens...").toTex());
 		
 		//Bewertung des Tutoriums
 		result.append("%==============================================================================\r\n\\section{Bewertung des Tutoriums}\r\n%==============================================================================\n");
-		result.append(questions.get("War der Tutor/die Tutorin auﬂerhalb der ‹bung f¸r Fragen etc. erreichbar?").toTex());
+		result.append(questions.get("War der Tutor/die Tutorin au√üerhalb der √úbung f√ºr Fragen etc. erreichbar?").toTex());
 		result.append(questions.get("Waren die Korrekturen des Tutors/der Tutorin nachvollziehbar?").toTex());
-		result.append(questions.get("Wurde der Tutor/die Tutorin mit dem Stoff der ‹bung fertig?").toTex());
+		result.append(questions.get("Wurde der Tutor/die Tutorin mit dem Stoff der √úbung fertig?").toTex());
 		result.append(questions.get("Lohnt sich der Besuch des Tutoriums?").toTex());
 		
-		//Abschlieﬂende Bewertung
-		result.append("%==============================================================================\r\n\\section{Abschlieﬂende Bewertung des Moduls}\r\n%==============================================================================\n");
+		//Abschlie√üende Bewertung
+		result.append("%==============================================================================\r\n\\section{Abschlie√üende Bewertung des Moduls}\r\n%==============================================================================\n");
 		result.append(questions.get("Note:").toTex());
 		
 		//Freitextkommentare
 		result.append("%==============================================================================\r\n\\section{Freitextkommentare}\r\n%==============================================================================\n");
 		result.append(questions.get("Was hat dir an dieser Lehrveranstaltung gefallen?").toTex());
-		result.append(questions.get("Was kˆnnte noch besser gemacht werden?").toTex());
-		result.append(questions.get("Hier hast du Platz f¸r weitere Anmerkungen und Feedback zum Modul.").toTex());
-		result.append(questions.get("Hier hast du Platz f¸r Anmerkungen und Feedback zur Umfrage.").toTex());
+		result.append(questions.get("Was k√∂nnte noch besser gemacht werden?").toTex());
+		result.append(questions.get("Hier hast du Platz f√ºr weitere Anmerkungen und Feedback zum Modul.").toTex());
+		result.append(questions.get("Hier hast du Platz f√ºr Anmerkungen und Feedback zur Umfrage.").toTex());
 		//---
 		result.append("\\end{document}");
 		return result.toString();
