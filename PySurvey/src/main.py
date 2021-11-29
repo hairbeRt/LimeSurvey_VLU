55a9b08e8df47ca21c4833f791019dd1934d4e44
hairbeRt <32942742+hairbeRt@users.noreply.github.com>
2021-11-29 10:58:58 +0100

    Compatibility for py 3.8

--- PySurvey/src/main.py
+++ PySurvey/src/main.py
@@ -9,5 +9,5 @@ def get_question_names(survey_data):
 if __name__ == '__main__':
     csv_files = glob.glob("data/*.csv")
     for file in csv_files:
-        basic_filename = os.path.basename(file).removesuffix(".csv")
+        basic_filename = os.path.basename(file).replace(".csv","")
         data_to_tex.generate_tex(basic_filename.replace("_", " "), "WS 21/22", "template/tex.cfg", "tex_docs/"+str(basic_filename)+".tex", file)
